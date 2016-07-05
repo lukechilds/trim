df74d19be6c248e5394c578009b3d99dfc89d792
Luke Childs <lukechilds123@gmail.com>
2016-07-05 23:05:39 +0100

    Print input from stdin

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::io;
+use std::io::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let stdin = io::stdin();
+    for line in stdin.lock().lines() {
+        println!("{}", line.unwrap());
+    }
 }
